0c42f8779afb44271a650f152d73d8941b276f5e
wenjus26 <rejuste.wenoumi@epitech.eu>
2026-07-03 22:28:03 +0100

    feat: implement custom premium 404 and 500 templates with brand animations and home redirects

--- filtec_site/urls.py
+++ filtec_site/urls.py
@@ -5,6 +5,10 @@ from django.conf.urls.static import static
 
 from django.views.generic import TemplateView
 
+# Custom Error Handlers
+handler404 = 'django.views.defaults.page_not_found'
+handler500 = 'django.views.defaults.server_error'
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('ckeditor/', include('ckeditor_uploader.urls')),
